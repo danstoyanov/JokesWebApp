129936935d7ed99773747eb9feeaafe6669f025c
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-01-11 18:01:11 +0200

    update

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public Joke()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string JokeQustion { get; set; }
+        public string JokeAnswer { get; set; }
+    }
+}
